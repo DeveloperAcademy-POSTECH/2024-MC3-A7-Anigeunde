337d5fe5c5ec2b354f804ce78ba20bda8ec1849a
fine <92whdtjs@gmail.com>
2024-07-31 23:00:15 +0900

    Feat: #17 - AudioManager
    - 현재 사용가능한 오디오 장치 목록을 관리합니다.
    - 오디오 장치 목록을 이용해 사용자가 원하는 장치를 선택할 수 있습니다.
    - 각 오디오 장치의 gain 여부를 확인하고 조절이 가능한 경우 gain 조정이 가능합니다.
    - 선택한 장치의 연결에 해제되었을 때 감지합니다.
    - 오디오 장치와 관련한 변경사항을 화면에 반영합니다.

--- Lvlance/Lvlance/Manager/AudioManaging/Model/Device.swift
+++ Lvlance/Lvlance/Manager/AudioManaging/Model/Device.swift
@@ -5,4 +5,17 @@
 //  Created by 이종선 on 7/31/24.
 //
 
-import Foundation
+
+struct Device: Hashable, Identifiable {
+    static let invalid = Device(id: "-1", name: "No audio device available", hasGainControl: false, currentGain: 0, minGain: 0, maxGain: 1)
+    let id: String
+    let name: String
+    var hasGainControl: Bool
+    var currentGain: Float
+    var minGain: Float
+    var maxGain: Float
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.id == rhs.id
+    }
+}
