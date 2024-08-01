84bc4b67d32ded0fb93db7130318a564ab4893a6
jiyoungsophia <sophiathedev@gmail.com>
2024-08-01 21:28:57 +0900

    Chore: #22- 엔티티 옵셔널 추가

--- Lvlance/InstrumentEntity+CoreDataProperties.swift
+++ Lvlance/InstrumentEntity+CoreDataProperties.swift
@@ -16,7 +16,7 @@ extension InstrumentEntity {
         return NSFetchRequest<InstrumentEntity>(entityName: "InstrumentEntity")
     }
 
-    @NSManaged public var instrumentId: UUID
-    @NSManaged public var type: String
+    @NSManaged public var instrumentId: UUID?
+    @NSManaged public var type: String?
 
 }
