40cf284e89d8d20604b4b06eb3a8e184b65db13f
fine <92whdtjs@gmail.com>
2024-08-06 23:41:11 +0900

    Feat: #30 - MicSelectView
    해당 Sheet뷰에서 마이크를 변경할 수 있습니다.

--- Lvlance/Lvlance/Views/MicSelect/MicSelectView.swift
+++ Lvlance/Lvlance/Views/MicSelect/MicSelectView.swift
@@ -9,59 +9,57 @@ import SwiftUI
 
 struct MicSelectView: View {
     
-    @StateObject private var audioManager = AudioManager()
-    @State private var selectedDevice: Device = .invalid
-    
+    @EnvironmentObject var audioManager: AudioManager
+    @Binding var isShowingMicrophoneSelector : Bool
         
     var body: some View {
         VStack{
             Spacer().frame(height: 99)
-            Text(selectedDevice == Device.invalid ? "밴드의 전체 소리를 입력할 기기를 선택해주세요. (권장 기기: iPhone)" : "선택하신 디바이스가 밴드의 연주 소리를 입력합니다.")
+            Text(audioManager.selectedAudioDevice == Device.invalid ? "밴드의 전체 소리를 입력할 기기를 선택해주세요. (권장 기기: iPhone)" : "선택하신 디바이스가 밴드의 연주 소리를 입력합니다.")
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
             
-            Text(selectedDevice == Device.invalid ? "이 기기가 입력한 밴드의 소리는 악기별로 분류되어 앱 내에 반영됩니다." : "원하는 위치에 입력 기기를 놓아주세요.")
+            Text(audioManager.selectedAudioDevice == Device.invalid ? "이 기기가 입력한 밴드의 소리는 악기별로 분류되어 앱 내에 반영됩니다." : "원하는 위치에 입력 기기를 놓아주세요.")
                 .multilineTextAlignment(.center)
                 .padding(.horizontal, 20)
-                .fontWeight(selectedDevice == Device.invalid ? .light : .bold )
+                .fontWeight(audioManager.selectedAudioDevice == Device.invalid ? .light : .bold )
             Spacer().frame(height: 38)
             
             
-            Picker("", selection: $selectedDevice) {
+            Picker("", selection: $audioManager.selectedAudioDevice) {
                 ForEach(audioManager.audioDevices, id: \.self) { device in
                     Text(device.name).tag(device)
                 }
             }
             .labelsHidden()
             .padding([.leading, .bottom, .trailing], 0.0)
-            .frame(width: 212)
+            .frame(width: 250)
             .pickerStyle(.menu)
             
             Spacer().frame(height: 25)
             
             Button(action: {
                 //마이크 설정 저장
+                isShowingMicrophoneSelector = false
             }) {
                 Text("곡 선택하기")
                     .multilineTextAlignment(.center)
                     .frame(width: 212, height: 24)
-                    .background(selectedDevice == Device.invalid ? Color.gray : Color.accentColor)
                     .cornerRadius(6)
+                    .background(audioManager.selectedAudioDevice == .invalid ? .gray : .systemPurple)
                 
             }
             .frame(width: 212, height: 24)
             .cornerRadius(6)
+            .disabled(audioManager.selectedAudioDevice == .invalid)
             
             Spacer().frame(height: 80)
             
             HStack{
                 Button(action: {
-                    print("건너뛰기")
-                    //곡 선택 뷰 연결
+                    isShowingMicrophoneSelector = false
                 }) {
                     Text("건너뛰기")
-                        .foregroundColor(.black)
                 }
-                .background(.white)
                 .cornerRadius(6)
                 .help("입력 기기 설정을 할 필요가 없는 경우, 건너뛰기하세요")
 
@@ -69,6 +67,7 @@ struct MicSelectView: View {
             }
             .padding(.bottom, 17)
         }
+        .tint(.systemPurple)
         .shadow(radius: 10)
         .padding(.horizontal, 20)
         .frame(width: 558, height: 364)
@@ -78,5 +77,5 @@ struct MicSelectView: View {
 
 
 #Preview {
-    MicSelectView()
+    MicSelectView( isShowingMicrophoneSelector: .constant(true))
 }
